354dacaf74fc63f84214a6e7b73c4a6c147df53c
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2018-02-14 20:40:29 -0500

    better prompts

--- play_i/player/console_player.py
+++ play_i/player/console_player.py
@@ -5,6 +5,7 @@ from play_i.player.player import Player
 
 class ConsolePlayer(Player):
     def make_choice(self, options):
-        print(options)
+        text_options = (f'{ind}: {x}' for ind, x in enumerate(options))
+        print(', '.join(text_options))
         choice = int(input('Choose the index of your move: '))
         return options[choice]
