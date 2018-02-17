f8e6729ac3e16c1b4e5ac2ca6c13bf2a0ccb570f
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2018-02-17 18:59:11 -0500

    CI testing w/ circleCI auto test coverage w/ coverage.py

---
+++ tests/play_i/games/tic_tac_toe/test_tic_tac_toe.py
@@ -0,0 +1,23 @@
+import unittest
+from play_i.games.tic_tac_toe.tic_tac_toe import TicTacToe
+from play_i.player.min_max_player import MinMaxPlayer
+from play_i.player.console_player import ConsolePlayer
+from play_i.player.random_player import RandomPlayer
+
+
+class TestTicTacToe(unittest.TestCase):
+
+    def testAdd(self):  # test method names begin with 'test'
+        self.assertEqual((1 + 2), 3)
+        self.assertEqual(0 + 1, 1)
+
+    def testMultiply(self):
+        self.assertEqual((0 * 10), 0)
+        self.assertEqual((5 * 8), 40)
+
+    def testLine(self):
+        # test
+        t = TicTacToe()
+        t.add_player(RandomPlayer())
+        t.add_player(RandomPlayer())
+        t.play()
